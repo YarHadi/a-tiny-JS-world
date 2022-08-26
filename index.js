da4da5ea405586fe3d178c3e00bab2dbc6cfe185
Yarik Hadion <110093395+YarHadi@users.noreply.github.com>
2022-08-26 17:38:38 +0300

    adding code in index.js

--- index.js
+++ index.js
@@ -1,31 +1,63 @@
 /* Refer to https://github.com/OleksiyRudenko/a-tiny-JS-world for the task details
    Complete the below for code reviewers' convenience:
 
-   Code repository: _put repo URL here_
-   Web app: _put project's github pages URL here_
+   Code repository: https://github.com/YarHadi/a-tiny-JS-world
+   Web app: https://yarhadi.github.io/a-tiny-JS-world/
    */
 
 // ======== OBJECTS DEFINITIONS ========
 // Define your objects here
 
+const dog = {
+   species: "dog",
+   name: "Mitsu",
+   gender: "female",
+   legs: 4,
+   hands: 0,
+   saying: "Woof-Woof!",
+};
+    
+const cat = {
+   species: "cat",
+   name: "Flicker",
+   gender: "male",
+   legs: 4,
+   hands: 0,
+   saying: "Meow!",
+};
+    
+const woman = {
+   species: "human",
+   name: "Alice",
+   gender: "female",
+   legs: 2,
+   hands: 2,
+   saying: "Hello!",
+};
+    
+const catWoman = {
+   species: "human",
+   name: "Diana",
+   gender: "female",
+   legs: 2,
+   hands: 2,
+   saying: cat.saying,
+};
 
-// ======== OUTPUT ========
-/* Use print(message) for output.
-   Default tag for message is <pre>. Use print(message,'div') to change containing element tag.
+const man = {
+   species: "human",
+   name: "Billy",
+   gender: "male",
+   legs: 2,
+   hands: 2,
+   saying: "Ass we can!",
+};
+    
+const inhabitants = [dog, cat, woman, catWoman, man];
 
-   Message can contain HTML markup. You may also tweak index.html and/or styles.css.
-   However, please, REFRAIN from improving visuals at least until your code is reviewed
-   so code reviewers might focus on a single file that is index.js.
-   */
+const keys = ["species", "name", "gender", "legs", "hands", "saying"];
 
-/* Print examples:
-   print('ABC');
-   print('<strong>ABC</strong>');
-   print('<strong>ABC</strong>', 'div');
-
-   print('human; John; male; 2; 2; Hello world!; Rex, Tom, Jenny');
-   print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny');
-   print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny', 'div');
-   */
-
+inhabitants.map((inhabitant) => {
+   print(keys.map((key) => inhabitant[key]).join(", "));
+});
 
